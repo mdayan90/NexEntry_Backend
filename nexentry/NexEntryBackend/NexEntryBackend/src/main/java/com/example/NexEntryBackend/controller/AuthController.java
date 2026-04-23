97f40454f0329ec3eed089b9940081bb8fb6fa7c
hashmiarsa <hashmiayan90@gmail.com>
2026-04-23 22:49:28 +0530

    my commit in mapping

--- nexentry/NexEntryBackend/NexEntryBackend/src/main/java/com/example/NexEntryBackend/controller/AuthController.java
+++ nexentry/NexEntryBackend/NexEntryBackend/src/main/java/com/example/NexEntryBackend/controller/AuthController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/auths")
 public class AuthController {
 
     @Autowired
